1636f61194143d086a9db98165bd261b9aefd48e
Hu Qiu <huqiu@fameeting.com>
2016-12-05 14:01:09 +0800

    添加setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(name='myblog',
+      version='1.0',
+      description='a personal blog with flask and mysql',
+      author='VincentHq',
+      author_email='huqiu@fsmeeting.com'
+      )
